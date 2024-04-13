e3405279e8621a58338bb935e294eccbd847bf77
Abdullah Özmantar <157515815+abdozmantar@users.noreply.github.com>
2024-04-13 14:58:12 +0300

    Update codeformer_arch.py

--- scripts/r_archs/codeformer_arch.py
+++ scripts/r_archs/codeformer_arch.py
@@ -25,7 +25,6 @@ def calc_mean_std(feat, eps=1e-5):
     feat_mean = feat.view(b, c, -1).mean(dim=2).view(b, c, 1, 1)
     return feat_mean, feat_std
 
-
 def adaptive_instance_normalization(content_feat, style_feat):
     """Adaptive instance normalization.
 
@@ -94,7 +93,6 @@ def _get_activation_fn(activation):
         return F.glu
     raise RuntimeError(F"activation should be relu/gelu, not {activation}.")
 
-
 class TransformerSALayer(nn.Module):
     def __init__(self, embed_dim, nhead=8, dim_mlp=2048, dropout=0.0, activation="gelu"):
         super().__init__()
@@ -154,7 +152,6 @@ class Fuse_sft_block(nn.Module):
         residual = w * (dec_feat * scale + shift)
         out = dec_feat + residual
         return out
-
 
 @ARCH_REGISTRY.register()
 class CodeFormer(VQAutoEncoder):
