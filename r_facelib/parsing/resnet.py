b6ce33f08d454461632a7927d1047e5d5477426d
Abdullah Özmantar <157515815+abdozmantar@users.noreply.github.com>
2024-06-18 12:27:48 +0300

    Update resnet.py

--- r_facelib/parsing/resnet.py
+++ r_facelib/parsing/resnet.py
@@ -5,8 +5,7 @@ import torch.nn.functional as F
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-
-
+    
 class BasicBlock(nn.Module):
 
     def __init__(self, in_chan, out_chan, stride=1):
